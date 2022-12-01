aa32030a8147e40b216759ce8a853358fdcef163
wilfried <wilfriedpapt@gmail.com>
2022-12-01 19:31:58 +0200

    pull=fetch+merge

---
+++ pull-all-remote-changes.sh
@@ -0,0 +1,8 @@
+#download on your local machine all remote changes
+git fetch
+
+# merge all remote changes with your local changes
+git merge
+
+#download remote changes and merge with your local changes
+(git fetch + git merge) = git pull
