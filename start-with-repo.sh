6573d63446f03ee34bc4f275d1f03b7e5c924b50
wilfried <wilfriedpapt@gmail.com>
2022-12-01 19:24:11 +0200

    start-wtih-a-repo

---
+++ start-with-repo.sh
@@ -0,0 +1,7 @@
+# When we have created a repo in our local environnement 
+#with git init and we want to sync with remote repo
+
+
+git remote add origin https://github.com/nom_du_repo.git
+git branch -M branch_name
+git push -u origin branch_name
